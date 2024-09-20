1abc44982240ea590b743d30ef811adab12b9872
Arnold Sujan Katru <96995931+ASKHOPE@users.noreply.github.com>
2024-09-20 22:40:48 +0530

    update /

--- routes/index.js
+++ routes/index.js
@@ -2,5 +2,6 @@ const express = require('express');
 const router = express.Router();
 
 router.use('/contacts', require('./contacts'));
+router.get('/',(req,res) => {res.send('Hello World')});
 
 module.exports = router;
